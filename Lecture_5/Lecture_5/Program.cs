213023e1cfc97581f216f67ee8cde416e96416a8
Pavel Litvinovich <21asdf@mail.ru>
2019-12-14 22:36:04 +0300

    switch

--- Lecture_5/Lecture_5/Program.cs
+++ Lecture_5/Lecture_5/Program.cs
@@ -6,8 +6,9 @@ namespace Lecture._5
     {
         static void Main(string[] args)
         {
+            
             int real_hour = Convert.ToInt32(DateTime.Now.Hour);
-
+            /*
             if (real_hour >= 9 && real_hour < 12)
             {
                 Console.WriteLine("Good morning, guys!");
@@ -23,6 +24,42 @@ namespace Lecture._5
             if (real_hour >= 22 && real_hour < 9)
             {
                 Console.WriteLine("Go to sleep!!!");
+            }
+            */
+            switch (real_hour)
+            {
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Good morning, guys!");
+                    break;
+                case 12:
+                case 13:
+                case 14:
+                    Console.WriteLine("Good day, guys!");
+                    break;
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                case 19:
+                case 20:
+                case 21:
+                    Console.WriteLine("Good evening, guys!");
+                    break;
+                case 22:
+                case 23:
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                case 6:
+                case 7:
+                case 8:
+                    Console.WriteLine("Go to sleep!!!");
+                    break;
             }
             Console.ReadKey();
         }
