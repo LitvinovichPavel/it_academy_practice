223f9fa43aea48a3d457ebbc4e448ebb7db4e15f
Pavel Litvinovich <21asdf@mail.ru>
2019-12-14 21:12:33 +0300

    Lecture5

---
+++ Lecture_5/Lecture_5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace Lecture._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int real_hour = Convert.ToInt32(DateTime.Now.Hour);
+
+            if (real_hour >= 9 && real_hour < 12)
+            {
+                Console.WriteLine("Good morning, guys!");
+            }
+            if (real_hour >= 12 && real_hour < 15)
+            {
+                Console.WriteLine("Good day, guys!");
+            }
+            if (real_hour >= 15 && real_hour < 22)
+            {
+                Console.WriteLine("Good evening, guys!");
+            }
+            if (real_hour >= 22 && real_hour < 9)
+            {
+                Console.WriteLine("Go to sleep!!!");
+            }
+            Console.ReadKey();
+        }
+    }
+}
