bcbbc348dce8fe3b498bcc14e78440170a117afa
Pavel Litvinovich <21asdf@mail.ru>
2019-12-19 20:44:11 +0300

    letterA

---
+++ Lecture_6/Lecture_6/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Letters_A
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Ведите слово: ");
+            string word;
+            word = Console.ReadLine();
+            string letterA = "а";
+            int count = 0;
+            int i = 0;
+
+            while ((i = word.IndexOf(letterA, i)) != -1)
+            {
+                i += letterA.Length;
+                count++;
+            }
+
+            Console.WriteLine($"В слове {word} буква а встречается {count} раз");
+            Console.ReadLine();
+        }
+    }
+}
