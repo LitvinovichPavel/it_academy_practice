e527f02d75a67842207d9fb4af809a1559e84ff0
Pavel Litvinovich <21asdf@mail.ru>
2019-12-24 19:12:53 +0300

    lecture6.Task1

---
+++ Lecture_07.Task1/Lecture_07.Task1/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Lecture_07.Task1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] arr = new int[0];
+        }
+    }
+}
